2f3355a828c6fac9c48a45fdc79c2081857e788f
Akloo-jamilu <jhasleg126@yahoo.com>
2022-12-17 21:19:12 +0100

    Api route feature test

---
+++ tests/Feature/Api/v1/Post/PostApiTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Api\v1\Post;
+
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostApiTest extends TestCase
+{
+    public function test_index()
+    {
+        $Posts = Post::factory(19)->create();
+        $postIds = $Posts->map(fn ($post)=> $post->id);
+        $response = $this->json('get', '/api/v1/posts');
+
+        $response->assertStatus(200);
+        $data = $response->json('data');
+        collect($data)->each(fn($post) => $this->assertTrue(in_array($post['id'], $postIds->toArray())));
+    }
+}
